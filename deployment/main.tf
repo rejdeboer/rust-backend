eab274fca60c3f4b0542132ed10277398cda987d
Rick de Boer <rick.deboer@live.nl>
2023-05-06 15:53:59 +0200

    feat: subscription email and name validation

--- deployment/main.tf
+++ deployment/main.tf
@@ -22,32 +22,3 @@ resource "azurerm_resource_group" "resource_group" {
   location = "northeurope"
 }
 
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "rust-api-cluster"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  dns_prefix          = "rust-api-cluster"
-
-  default_node_pool {
-    name       = "default"
-    node_count = "1"
-    vm_size    = "standard_d2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  addon_profile {
-    http_application_routing {
-      enabled = true
-    }
-  }
-}
-
-resource "azurerm_kubernetes_cluster_node_pool" "mem" {
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster.id
-  name                  = "mem"
-  node_count            = "1"
-  vm_size               = "standard_d11_v2"
-}
